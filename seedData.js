c84dccdc598e51605f073ed259d50ecbba48b76b
Joyce-Wong <joycekwo12@gmail.com>
2023-02-16 10:49:38 -0500

    graphql tested in browser mutation works

---
+++ seedData.js
@@ -0,0 +1,20 @@
+const actors = [
+    {id: 1, name: "Robin Williams"},
+    {id: 2, name: "Pedro Pascal"},
+    {id: 3, name: "Jennifer Lawrence"}
+];
+
+const movies = [
+    {id: 1, name: "iRobot", actorId: 1},
+    {id: 2, name: "Good Will Hunting", actorId: 1},
+    {id: 2, name: "The Pursuit of Happyness", actorId: 2},
+    {id: 2, name: "Home Alone", actorId: 3},
+    {id: 2, name: "Happy Gilmore", actorId: 1},
+    {id: 2, name: "Batman", actorId: 1},
+    {id: 2, name: "Parasite", actorId: 1},
+]
+
+module.exports = {
+    actors,
+    movies
+}
